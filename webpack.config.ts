6ef9469385b80d5747daabf6790ca6c820668ede
Lucas Navarro <lucas.navarro@lilab.pe>
2023-12-20 07:36:45 -0300

    style: update the tab linter on prettier

--- webpack.config.ts
+++ webpack.config.ts
@@ -4,42 +4,43 @@ import HtmlWebpackPlugin from "html-webpack-plugin";
 import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
 import ESLintPlugin from "eslint-webpack-plugin";
 import {TsconfigPathsPlugin} from "tsconfig-paths-webpack-plugin";
+import packageJson from "./package.json";
 
 const webpackConfig = (env): Configuration => ({
-    entry: "./src/index.tsx",
-    ...(env.production || !env.development ? {} : {devtool: "eval-source-map"}),
-    resolve: {
-        extensions: [".ts", ".tsx", ".js"],
-        plugins: [new TsconfigPathsPlugin()]
-    },
-    output: {
-        path: path.join(__dirname, "/dist"),
-        filename: "build.js"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                loader: "ts-loader",
-                options: {
-                    transpileOnly: true
-                },
-                exclude: /dist/
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./public/index.html"
-        }),
-        new webpack.DefinePlugin({
-            "process.env.PRODUCTION": env.production || !env.development,
-            "process.env.NAME": JSON.stringify(require("./package.json").name),
-            "process.env.VERSION": JSON.stringify(require("./package.json").version)
-        }),
-        new ForkTsCheckerWebpackPlugin(),
-        new ESLintPlugin({files: "./src/**/*.{ts,tsx,js,jsx}"})
+  entry: "./src/index.tsx",
+  ...(env.production || !env.development ? {} : {devtool: "eval-source-map"}),
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+    plugins: [new TsconfigPathsPlugin()]
+  },
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "build.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        options: {
+          transpileOnly: true
+        },
+        exclude: /dist/
+      }
     ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html"
+    }),
+    new webpack.DefinePlugin({
+      "process.env.PRODUCTION": env.production || !env.development,
+      "process.env.NAME": JSON.stringify(packageJson.name),
+      "process.env.VERSION": JSON.stringify(packageJson.version)
+    }),
+    new ForkTsCheckerWebpackPlugin(),
+    new ESLintPlugin({files: "./src/**/*.{ts,tsx,js,jsx}"})
+  ]
 });
 
 export default webpackConfig;
