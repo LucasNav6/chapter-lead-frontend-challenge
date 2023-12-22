df61882f319a35991cb3d8edbd88569cdcd62003
Lucas Navarro <lucas.navarro@lilab.pe>
2023-12-22 08:40:59 -0300

    feat: add bases auth layout

--- src/pages/layout/Auth.layout.tsx
+++ src/pages/layout/Auth.layout.tsx
@@ -2,7 +2,11 @@ import {IAuthLayoutProps} from "@Models/interfaces/layouts";
 import React from "react";
 
 const AuthLayout: React.FC<IAuthLayoutProps> = ({children}) => {
-  return <main>{children}</main>;
+  return (
+    <main className="auth-layout-main">
+      <section className="auth-layout-container">{children}</section>
+    </main>
+  );
 };
 
 export default AuthLayout;
