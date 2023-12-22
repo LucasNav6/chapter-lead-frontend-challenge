c277371e9c206c5245e3d45d54a06466a3013f93
Lucas Navarro <lucas.navarro@lilab.pe>
2023-12-22 08:38:15 -0300

    refactor: update router app

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
-import App from "@Pages/App";
+import App from "@Routes/App.routes";
 import React from "react";
 import {createRoot} from "react-dom/client";
+import "./scss/global.scss";
 
 const root = document.getElementById("root");
 const rootInstance = createRoot(root);
