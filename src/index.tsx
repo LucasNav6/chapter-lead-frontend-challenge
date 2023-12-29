504d946912ece771300d053e3ba2fcf741ed097b
Lucas Navarro <lucas.navarro@lilab.pe>
2023-12-29 15:43:28 -0300

    refactor: update the main root

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,11 @@ import React from "react";
 import {createRoot} from "react-dom/client";
 import "./scss/global.scss";
 
-const root = document.getElementById("root");
-const rootInstance = createRoot(root);
+// Get the root element of the app to render
+// If the root element is not found, throw an error
+const rootEl = document.getElementById("root");
+if (!rootEl) throw new Error("Root element not found");
+
+// Creating a React root instance and rendering the main App component
+const rootInstance = createRoot(rootEl);
 rootInstance.render(<App />);
