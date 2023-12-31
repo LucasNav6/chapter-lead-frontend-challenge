7e10763a3348734bb4c004d5fa467a583b56d95b
Lucas Navarro <lucas.navarro@lilab.pe>
2023-12-30 21:23:08 -0300

    style: remove console.log

--- src/routes/hooks/useOnUserAuthorized.tsx
+++ src/routes/hooks/useOnUserAuthorized.tsx
@@ -5,8 +5,6 @@ const useRedirectUserWhen = () => {
   // Check if user is authorized
   const {user_uuid} = useStore();
 
-  console.log("user_uuid", user_uuid);
-
   // If user is not authorized, return false
   return user_uuid ? true : false;
 };
