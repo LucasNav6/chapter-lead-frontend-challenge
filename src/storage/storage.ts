934acc2f9500d55057eb66d14a2eca9e39347db9
Lucas Navarro <lucas.navarro@lilab.pe>
2024-01-03 08:04:04 -0300

    refactor: create an interface
    Create an interface of the initialState and the store. Add comments too

--- src/storage/storage.ts
+++ src/storage/storage.ts
@@ -1,35 +1,39 @@
-import {ITask} from "@Adapters/tasks/createTaskIntoProject";
-import {ProjectItem} from "src/data";
+import {ProjectItem} from "@Models/interfaces/projects";
 import {create} from "zustand";
 import {persist} from "zustand/middleware";
 
-type Store = {
+// Define the structure of the store
+type TStoreInitialState = {
   user_uuid?: string | null;
+  user_mail?: string | null;
+  projects?: ProjectItem[];
+};
+interface IStore extends TStoreInitialState {
   removeUserUUID?: () => void;
   setUserUUID?: (uuid: string) => void;
-  user_mail?: string | null;
   removeUserMail?: () => void;
   setUserMail?: (mail: string) => void;
-  projects?: ProjectItem[];
   setProjects?: (projects: ProjectItem[]) => void;
-  setTaskIntoProject?: (projectId: string, task: ITask) => void;
   logOut?: () => void;
+}
+
+// Create the Zustand store
+const initialState: TStoreInitialState = {
+  user_uuid: null,
+  user_mail: null,
+  projects: []
 };
 
-const useStore: () => Store = create(
+const useStore: () => IStore = create(
   persist(
     (set) => ({
-      user_uuid: null,
+      ...initialState,
       removeUserUUID: () => set({user_uuid: null}),
       setUserUUID: (uuid) => set({user_uuid: uuid}),
-      user_mail: null,
       removeUserMail: () => set({user_mail: null}),
       setUserMail: (mail) => set({user_mail: mail}),
-      projects: [],
       setProjects: (projects) => set({projects: projects}),
-      logOut: () => {
-        set({user_uuid: null, user_mail: null, projects: []});
-      }
+      logOut: () => set(initialState)
     }),
     {
       name: "user-storage"
@@ -38,17 +42,3 @@ const useStore: () => Store = create(
 );
 
 export default useStore;
-
-// const useStore = create<Store>()((set) => ({
-//   // Manage the UUID state of the user
-//   user_uuid: null,
-//   removeUserUUID: () => set({user_uuid: null}),
-//   setUserUUID: (uuid: string) => set({user_uuid: uuid}),
-
-//   // Manage the user mail
-//   user_mail: null,
-//   removeUserMail: () => set({user_mail: null}),
-//   setUserMail: (mail: string) => set({user_mail: mail})
-// }));
-
-// export default useStore;
