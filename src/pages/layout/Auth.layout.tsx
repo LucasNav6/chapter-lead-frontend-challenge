637e1e837153ec4297a727568961b185876f470a
Lucas Navarro <lucas.navarro@lilab.pe>
2024-01-03 08:09:11 -0300

    perf: add toaster into the layouts

--- src/pages/layout/Auth.layout.tsx
+++ src/pages/layout/Auth.layout.tsx
@@ -1,9 +1,11 @@
 import {IAuthLayoutProps} from "@Models/interfaces/layouts";
 import React from "react";
+import {Toaster} from "react-hot-toast";
 
 const AuthLayout: React.FC<IAuthLayoutProps> = ({children}) => {
   return (
     <main className="auth-layout-main">
+      <Toaster position="bottom-center" reverseOrder={true} />
       <section className="auth-layout-container">{children}</section>
     </main>
   );
--- src/pages/layout/Main.layout.tsx
+++ src/pages/layout/Main.layout.tsx
@@ -1,9 +1,11 @@
 import {IMainLayoutProps} from "@Models/interfaces/layouts";
 import React from "react";
+import {Toaster} from "react-hot-toast";
 
 const MainLayout: React.FC<IMainLayoutProps> = ({children}) => {
   return (
-    <main className="main-layout-main">
+    <main className="main-layout">
+      <Toaster position="bottom-center" reverseOrder={true} />
       <section className="main-layout-container">{children}</section>
     </main>
   );
